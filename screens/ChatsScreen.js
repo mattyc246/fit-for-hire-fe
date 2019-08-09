0fd170ed4cdb70e7aa440a6ad74a37ce50194090
Matthew Cross <mattyc246@gmail.com>
2019-08-09 16:26:28 +0800

    chat sockets connected and tested working

--- screens/ChatsScreen.js
+++ screens/ChatsScreen.js
@@ -1,18 +1,58 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import {
+  ScrollView,
+  StyleSheet,
+  Alert,
+  AsyncStorage,
+  View,
+  Text,
+  Dimensions
+} from "react-native";
+import socketIO from "socket.io-client";
+import Axios from "axios";
 
-export default function ChatsScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+class ChatsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chats: []
+    };
+  }
+
+  componentDidMount = async () => {
+    let jwt = await AsyncStorage.getItem("userTokenF4H");
+
+    let config = {
+      headers: {
+        Authorization: `Bearer ${jwt}`
+      }
+    };
+
+    Axios.get("http://192.168.1.71:5000/api/v1/chats/active_chats", config)
+      .then(response => {
+        this.setState({ chats: response.data.chats });
+      })
+      .catch(error => {
+        Alert.alert("Error", "Something went wrong :(");
+      });
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {this.state.chats.length > 0 ? (
+          this.state.chats.map(chat => {
+            return <View style={styles.titleBox} />;
+          })
+        ) : (
+          <Text style={styles.noChatsText}>No Chats Available</Text>
+        )}
+      </ScrollView>
+    );
+  }
 }
+
+export default ChatsScreen;
 
 ChatsScreen.navigationOptions = {
   title: "Chats"
@@ -21,7 +61,28 @@ ChatsScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+    backgroundColor: "#cbe5f8"
+  },
+  titleBox: {
+    flex: 1,
+    flexDirection: "row",
+    width: "90%",
+    height: 50,
+    justifyContent: "space-around",
+    borderWidth: 1,
+    borderColor: "white",
+    borderRadius: 10,
+    alignSelf: "center",
+    marginTop: 20,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 15
+  },
+  noChatsText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginTop: Dimensions.get("window").height / 2.4,
+    textAlign: "center"
   }
 });
